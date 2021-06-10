f7659b10510bcffd7a0c5d481e66bf5a5089eb96
Yuunan Kin <yuunan.kin@gmail.com>
2021-06-10 14:35:30 +0900

    example of using layout widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // const questions = ['Question 1', 'Question 2'];
     return MaterialApp(
         title: 'QuizApp',
         home: Scaffold(
           appBar: AppBar(
             title: Text('Quiz App'),
           ),
-          body: Text('Content of Quiz App'),
+          body: Column(
+            children: [
+              Text('First Question'),
+              ElevatedButton(
+                child: Text('Answer 1'),
+                onPressed: () => {},
+              ),
+              ElevatedButton(
+                child: Text('Answer 2'),
+                onPressed: () => {},
+              ),
+              Text('Second Question'),
+            ],
+          ),
         ));
   }
 }
