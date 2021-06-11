f36ff7fa756b33d11e4c2c35befd3d71dce64d67
Yuunan Kin <yuunan.kin@gmail.com>
2021-06-12 08:17:02 +0900

    make properties private

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,24 @@ class QA {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  List<QA> qas = [
+class _MyAppState extends State<MyApp> {
+  List<QA> _qas = [
     QA('Q1', ['A1-1', 'A1-2']),
     QA('Q2', ['A2-1', 'A2-2', 'A2-3']),
     QA('Q3', ['A3-1', 'A3-2', 'A3-3']),
   ];
-  int questionIndex = 0;
-  void onAnswerSelected() {
-    print('questionIndex:$questionIndex');
-    if (questionIndex == qas.length - 1) {
+  int _questionIndex = 0;
+  void _onAnswerSelected() {
+    print('questionIndex:$_questionIndex');
+    if (_questionIndex == _qas.length - 1) {
       return;
     }
     setState(() {
-      questionIndex++;
+      _questionIndex++;
     });
   }
 
@@ -47,11 +47,11 @@ class MyAppState extends State<MyApp> {
           ),
           body: Column(
             children: [
-              Text(qas[questionIndex].question),
-              ...qas[questionIndex].answers.map((e) {
+              Text(_qas[_questionIndex].question),
+              ..._qas[_questionIndex].answers.map((e) {
                 return ElevatedButton(
                   child: Text(e),
-                  onPressed: onAnswerSelected,
+                  onPressed: _onAnswerSelected,
                 );
               })
             ],
